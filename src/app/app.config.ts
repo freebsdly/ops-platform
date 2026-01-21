efd75c252854f3209a6a181eeb7ab4f82a6fd1e3
qinhuajun <qinhuajun@outlook.com>
2026-01-21 22:17:13 +0800

    init

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,19 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { icons } from './icons-provider';
+import { provideNzIcons } from 'ng-zorro-antd/icon';
+import { zh_CN, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+
+registerLocaleData(zh);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideNzIcons(icons),
+    provideNzI18n(zh_CN),
+  ],
 };
