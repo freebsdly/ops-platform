7598d12a3e6c2743cf07bf50ab1415a6eeb39bc4
qinhuajun <qinhuajun@outlook.com>
2026-01-24 21:12:57 +0800

    update

--- src/app/layout.service.ts
+++ src/app/layout.service.ts
@@ -1,11 +1,42 @@
 import { Injectable, signal } from '@angular/core';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+export interface MenuItem {
+  text: string;
+  icon?: string;
+  link?: string;
+  children?: MenuItem[];
+  open?: boolean;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class LayoutService {
+  private router = inject(Router);
   isCollapsed = signal(false);
   private appVersion = signal('v1.0.0');
+  menuData = signal<MenuItem[]>([
+    {
+      text: 'Dashboard',
+      icon: 'dashboard',
+      open: true,
+      children: [
+        { text: 'Welcome', link: '/welcome' },
+        { text: 'Monitor' },
+        { text: 'Workplace' },
+      ],
+    },
+    {
+      text: 'Form',
+      icon: 'form',
+      open: true,
+      children: [
+        { text: 'Basic Form' },
+      ],
+    },
+  ]);
 
   toggle() {
     this.isCollapsed.update((v) => !v);
@@ -17,5 +48,9 @@ export class LayoutService {
 
   getAppVersion() {
     return this.appVersion;
+  }
+
+  setMenuData(menuData: MenuItem[]) {
+    this.menuData.set(menuData);
   }
 }
--- src/app/layout/sider-menu/sider-menu.ts
+++ src/app/layout/sider-menu/sider-menu.ts
@@ -1,15 +1,23 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, ChangeDetectionStrategy } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 import { LayoutService } from '../../layout.service';
+import { MenuItem } from '../../layout.service';
 
 @Component({
   selector: 'app-sider-menu',
-  imports: [RouterLink, NzMenuModule],
+  imports: [RouterLink, NzMenuModule, NzIconModule],
   templateUrl: './sider-menu.html',
   styleUrl: './sider-menu.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SiderMenu {
   layoutService = inject(LayoutService);
   isCollapsed = this.layoutService.isCollapsed.asReadonly();
+  menuData = this.layoutService.menuData.asReadonly();
+
+  trackByMenuId(_index: number, item: MenuItem): string {
+    return item.text;
+  }
 }
