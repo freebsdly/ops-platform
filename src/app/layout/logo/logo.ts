190f3198c4bb9709a42e4a5dc8f04c75f8fbdc81
itoutsource.cz1731 <itoutsource.cz1731@trinasolar.com>
2026-02-03 13:46:52 +0800

    update

--- src/app/layout/logo/logo.ts
+++ src/app/layout/logo/logo.ts
@@ -1,6 +1,8 @@
-import { Component, input, computed } from '@angular/core';
+import { Component, input, computed, inject } from '@angular/core';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { LogoConfig } from '../../core/types/layout-config.interface';
+import { StoreService } from '../../core/stores/store.service';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-logo',
@@ -9,20 +11,30 @@ import { LogoConfig } from '../../core/types/layout-config.interface';
   styleUrl: './logo.css',
 })
 export class Logo {
+  private storeService = inject(StoreService);
+  
   isCollapsed = input.required<boolean>();
-  title = input('');
-  config = input<LogoConfig>();
+  
+  // 直接从StoreService获取状态
+  logoConfigSig = toSignal(this.storeService.logoConfig$, { 
+    initialValue: null 
+  });
   
   // 从配置派生属性
-  logoSrc = computed(() => this.config()?.src || 'https://ng.ant.design/assets.img/logo.svg');
-  logoAlt = computed(() => this.config()?.alt || 'Logo');
-  logoLink = computed(() => this.config()?.link || 'https://ng.ant.design/');
-  logoCollapsedIcon = computed(() => this.config()?.collapsedIcon || 'bars');
-  logoExpandedIcon = computed(() => this.config()?.expandedIcon || 'bars');
+  logoSrc = computed(() => this.logoConfigSig()?.src || 'https://ng.ant.design/assets.img/logo.svg');
+  logoAlt = computed(() => this.logoConfigSig()?.alt || 'Logo');
+  logoLink = computed(() => this.logoConfigSig()?.link);
+  logoWidth = computed(() => this.logoConfigSig()?.width || '32px');
+  logoHeight = computed(() => this.logoConfigSig()?.height || '32px');
+  isVisible = computed(() => this.logoConfigSig()?.visible ?? true);
+  logoCollapsedIcon = computed(() => this.logoConfigSig()?.collapsedIcon || 'bars');
+  logoExpandedIcon = computed(() => this.logoConfigSig()?.expandedIcon || 'bars');
+  logoTitle = computed(() => this.logoConfigSig()?.alt || 'Logo');
   
-  showTitle = computed(() => !this.isCollapsed() && !!this.title());
-  showImage = computed(() => !!this.logoSrc());
-  showIcon = computed(() => !this.logoSrc() || this.isCollapsed());
+  // 显示逻辑
+  showTitle = computed(() => !this.isCollapsed() && !!this.logoTitle());
+  showImage = computed(() => this.isVisible() && !!this.logoSrc());
+  showIcon = computed(() => this.isVisible() && (!this.logoSrc() || this.isCollapsed()));
   iconType = computed(() => 
     this.isCollapsed() 
       ? this.logoCollapsedIcon() || 'bars'
--- src/app/layout/sider-header/sider-header.ts
+++ src/app/layout/sider-header/sider-header.ts
@@ -1,6 +1,5 @@
-import { Component, input, computed } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { Logo } from '../logo/logo';
-import { LayoutConfig } from '../../core/types/layout-config.interface';
 
 @Component({
   selector: 'app-sider-header',
@@ -9,13 +8,6 @@ import { LayoutConfig } from '../../core/types/layout-config.interface';
   styleUrl: './sider-header.css',
 })
 export class SiderHeader {
-  // 从父组件接收完整配置
-  layoutConfig = input.required<LayoutConfig>();
-  
   // 从父组件接收侧边栏状态
   isSiderCollapsed = input.required<boolean>();
-  
-  // 派生配置
-  appTitle = computed(() => this.layoutConfig()?.appTitle || 'Ops Platform');
-  logoConfig = computed(() => this.layoutConfig()?.logo);
 }
